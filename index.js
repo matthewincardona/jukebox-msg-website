174975a23b9fa2c2c110eff0816033a65c4d2a99
matthewincardona <doublethink.inc@gmail.com>
2024-05-05 22:47:03 -0400

    Change title

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ io.on("connection", (socket) => {
   console.log("a user connected");
 });
 
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+  });
+});
+
 server.listen(3000, () => {
   console.log("listening on *:3000");
 });
