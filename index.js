c6258b5af262b131d4ee2decffc839b90ff34b4c
matthewincardona <doublethink.inc@gmail.com>
2024-05-05 23:24:21 -0400

    Users can see each other's messages

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -11,11 +16,14 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("a user connected");
-});
 
-io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
     console.log("message: " + msg);
+    io.emit("chat message", msg); // broadcast our message to all clients
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
 });
 
